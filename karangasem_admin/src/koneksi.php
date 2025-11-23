0879e95ba9e99297bde58596246c00a7126e2b10
Muhammad Ivan Aldorino <ivanaldorino@gmail.com>
2025-11-23 17:33:58 +0700

    Karangasem Beta v1.1.0

--- karangasem_admin/src/koneksi.php
+++ karangasem_admin/src/koneksi.php
@@ -1,20 +1,24 @@
 <?php
-// Konfigurasi Database (Cross-Container)
 
-$host = "mysql_db";   // <--- Nama service dari Docker MySQL Pusat
-$user = "root";
-$pass = "aldorino04"; // Password dari Docker MySQL Pusat
-$db   = "karangasem"; // Pastikan database ini SUDAH ANDA BUAT di phpMyAdmin (localhost:9999)
-$port = 3306;         // <--- Gunakan Port 3306 (Port Internal antar container)
+$host = "100.115.160.110"; 
+$user = "root"; 
+$pass = "aldorino04"; 
+$db   = "karangasem"; 
 
-// Membuat koneksi
+
+$port = 3306; 
+
+
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Cek koneksi
+
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+
+    die("Koneksi ke CasaOS Gagal: (" . $conn->connect_errno . ") " . $conn->connect_error);
 }
 
-// Set charset
+
 $conn->set_charset("utf8mb4");
+
+echo "Berhasil terkoneksi ke Database CasaOS!";
 ?>
